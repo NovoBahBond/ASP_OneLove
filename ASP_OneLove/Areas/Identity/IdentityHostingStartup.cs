1daec8c5fd4a917cf6740e4776ede98db2517d29
MrDiviner <yevhenii.bakhmat@nure.ua>
2020-05-16 16:47:47 +0300

    Account features: Login/Logout/Register/ChangePassword/ChangeEmail/Profile/DeleteAccount/PasswordReset

---
+++ ASP_OneLove/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ASP_OneLove.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASP_OneLove.Areas.Identity.IdentityHostingStartup))]
+namespace ASP_OneLove.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
