8133223550bd2e4d85e1ec8a61c04f75638bf92e
MrDiviner <yevhenii.bakhmat@nure.ua>
2020-05-17 10:48:52 +0300

    Изменил иерархию, Пофиксил названия

---
+++ ASP_OneLove/Models/Products/Electronics/SlowCooker.cs
@@ -0,0 +1,30 @@
+﻿using ASP_OneLove.Models.Products.Enums;
+
+namespace ASP_OneLove.Models.Products.Electronics
+{
+    public class SlowCooker : Product // Мультиварка
+    {
+        public string Model { get; set; }
+
+        public Volume Volume { get; set; }
+
+        public int WeightInKg { get; set; }
+
+        public int PowerInWatt { get; set; }
+
+        public Language Language { get; set; }
+
+        public byte AmountOfPrograms { get; set; }
+
+        public Programs Programs { get; set; } // Empty
+
+        public bool ExistenceOfDisplay { get; set; }
+    }
+
+    public enum Programs // TODO: set names of programs
+    {
+
+    }
+    
+}
+
